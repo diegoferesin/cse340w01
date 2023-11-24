78c3f5b10cf6ddb2712fa01aadef7a1a177daab1
Diego Feresin <diegoferesin@gmail.com>
2023-11-24 00:35:13 -0300

    Update 404 message#

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use("/inv", inventoryRoute)
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
-  next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
+  next({ status: 404, message: '404: Error not found. Seriously, where did it go?' })
 })
 
 /* ***********************
